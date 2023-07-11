7ebf14766198aabbceb47eb975ce31a7b24f4944
kail-yamaguchi <kail@AF52-1355.localdomain>
2023-07-11 18:13:39 +0900

    oohaba hennkou: add app.js

---
+++ app.js
@@ -0,0 +1,63 @@
+const staticLoadPlaces = () => [
+  {
+    label: 'p1',
+    location: {
+      latitude:   35.664350,
+      longitude: 139.756175,
+    },
+  },
+];
+
+const models = [
+  {
+    url: '#animated-asset',
+    scale: ['0.5', '0.5', '0.5'],
+  },
+];
+
+const createEntity = ({ location: { latitude, longitude }, model, scale: [x, y, z] }) => {
+  const $entity = document.createRange().createContextualFragment(`
+    <a-entity
+      gltf-model="${model}"
+      scale="${x} ${y} ${z}"
+      gps-entity-place="latitude: ${latitude}; longitude: ${longitude};"
+    ></a-entity>
+  `)
+
+  $entity.addEventListener(
+    'loaded',
+    () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')),
+  );
+
+  return $entity;
+};
+
+const renderPlace = ({ location }) => {
+  const $scene = document.querySelector('a-scene');
+  const { url, scale} = models[0];
+  const $entity = createEntity({
+    location,
+    model: url, scale });
+  $scene.appendChild($entity);
+};
+
+const main = async () => {
+  console.log('main');
+
+  const successCallback = position => {
+    console.log('success', position);
+    staticLoadPlaces().forEach(renderPlace);
+  };
+
+  const errorCallback = error => {
+    console.log('error', error);
+    alert(error.message);
+  };
+
+  navigator.geolocation.getCurrentPosition(
+    successCallback,
+    errorCallback
+  );
+};
+
+window.onload = main;
